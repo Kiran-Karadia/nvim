a9ab2c6959ef2eb599a597abd2afdb1c46a6ac09
Kiran Karadia <kiran_karadia@hotmail.com>
2026-03-16 00:42:30 +0000

    More fzf pickers

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -7,7 +7,16 @@ require("nvim-web-devicons").setup()
 
 local fzf = require("fzf-lua")
 
-fzf.setup()
+local colors = { true }
+local winopts = {
+  height = 0.95,
+  width = 0.95,
+}
+
+fzf.setup {
+  fzf_colors = colors,
+  winopts = winopts,
+}
 
 
 local vk = vim.keymap
@@ -15,4 +24,24 @@ local vk = vim.keymap
 vk.set("n", "<leader>ff", function()
   fzf.files()
 end, { desc = "Find files" })
+
+vk.set("n", "<leader>fg", function()
+  fzf.live_grep()
+end, { desc = "Grep files" })
+
+vk.set("n", "<leader>fb", function()
+  fzf.buffers()
+end, { desc = "Find in all buffers" })
+
+vk.set("n", "<leader>fh", function()
+  fzf.history()
+end, { desc = "Find history" })
+
+vk.set("n", "<leader>fq", function()
+  fzf.quickfix()
+end, { desc = "Quickfix list" })
+
+vk.set("n", "<leader><C-f>", function()
+  fzf.blines()
+end, { desc = "Find in buffer" })
 
