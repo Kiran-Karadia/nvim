645e16a54b9f331b99be49fdd116e9bbc1f32ad7
Kiran Karadia <kiran_karadia@hotmail.com>
2026-03-16 23:08:14 +0000

    Update blink config back to more defaults

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -8,29 +8,11 @@ vim.pack.add({
 
 -- require("friendly-snippets").setup()
 require("blink.cmp").setup {
-  keymap = {
-    preset = 'default',
-    ['<C-n>'] = { 'show', 'select_next', 'fallback' },
-    ['<C-p>'] = { 'show', 'select_prev', 'fallback' },
-  },
   completion = {
-    trigger = {
-      show_on_keyword = false,
-      show_on_trigger_character = false,
-    },
     documentation = {
       auto_show = true,
       auto_show_delay_ms = 200,
     },
-    list = {
-      selection = {
-        preselect = false,
-        auto_insert = true
-      },
-    },
-  },
-  sources = {
-    default = { 'lsp', 'path', 'snippets' },
   },
   signature = { enabled = true },
 }
